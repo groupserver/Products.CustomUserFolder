8b5a9c9d8e1aa8683d93209ac2b87f083dbafc2e
Richard Waid <richard@iopen.net>
2003-03-24 03:30:51 +0000

    Added framework for testing CustomUserFolder.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/CustomUserFolder/trunk%4059

---
+++ tests/testCustomUserFolder.py
@@ -0,0 +1,27 @@
+#
+# Skeleton ZopeTestCase
+#
+import os, sys
+if __name__ == '__main__':
+    execfile(os.path.join(sys.path[0], 'framework.py'))
+
+from Testing import ZopeTestCase
+
+ZopeTestCase.installProduct('CustomUserFolder')
+
+from Products.CustomUserFolder import CustomUserFolder
+class TestCustomUserFolder(ZopeTestCase.ZopeTestCase):
+    def afterSetUp(self):
+        pass
+        
+    def afterClear(self):
+        pass    
+
+if __name__ == '__main__':
+    framework(descriptions=1, verbosity=1)
+else:
+    import unittest
+    def test_suite():
+        suite = unittest.TestSuite()
+        suite.addTest(unittest.makeSuite(TestSomeProduct))
+        return suite

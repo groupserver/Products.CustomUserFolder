6b5308461e479a6e41db81f79f9fc8761d0afaba
Michael JasonSmith <mpj17@onlinegroups.net>
2008-01-25 01:45:54 +0000

    And unicode.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/CustomUserFolder/trunk%403137

--- CustomUserFolder.py
+++ CustomUserFolder.py
@@ -205,8 +205,8 @@ class CustomUserFolder(UserFolderWithGroups):
         
         """
         assert name, 'User ID not set'
-        assert type(name) == str, 'User ID is a %s not a string (%s)' %\
-          (type(name), name)
+        assert type(name) not in (str, unicode), \
+          'User ID is a %s not a string (%s)' % (type(name), name)
         user_folder = self._getUserFolder()
         user = getattr(user_folder, name, None)
         if user:

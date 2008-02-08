14371a80fc6977d026c5906ae5d91259ddeb5c82
Michael JasonSmith <mpj17@onlinegroups.net>
2008-02-08 01:06:40 +0000

    Changing a column name to something more sensible.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/CustomUserFolder/trunk%403198

--- sql/user_invitation.sql
+++ sql/user_invitation.sql
@@ -1,7 +1,7 @@
 CREATE TABLE user_invitation (
     INVITATION_ID     TEXT                        PRIMARY KEY,
     USER_ID           TEXT                        NOT NULL,
-    ADMIN_USER_ID     TEXT                        NOT NULL,
+    INVITING_USER_ID  TEXT                        NOT NULL,
     SITE_ID           TEXT                        NOT NULL,
     GROUP_ID          TEXT                        NOT NULL,
     INVITATION_DATE   TIMESTAMP WITH TIME ZONE    DEFAULT NULL
@@ -9,7 +9,6 @@ CREATE TABLE user_invitation (
 --=mpj17=-- There is no foreign key for user_id, yet
 
 --=mpj17=-- Each user can only be invited to each group once
-CREATE UNIQUE INDEX group_invite_idx ON user_invitation USING BTREE 
+CREATE UNIQUE INDEX group_invite_idx ON user_invitation USING BTREE
     (user_id, site_id, group_id);
-    
 

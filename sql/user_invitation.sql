df94912a84ff216b3f42195ae6ce739c69c09d93
Michael JasonSmith <mpj17@onlinegroups.net>
2008-03-06 23:20:38 +0000

    Dropping an index.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/CustomUserFolder/trunk%403301

--- sql/user_invitation.sql
+++ sql/user_invitation.sql
@@ -8,7 +8,3 @@ CREATE TABLE user_invitation (
 );
 --=mpj17=-- There is no foreign key for user_id, yet
 
---=mpj17=-- Each user can only be invited to each group once
-CREATE UNIQUE INDEX group_invite_idx ON user_invitation USING BTREE
-    (user_id, site_id, group_id);
-

08cc1080c239513951cf0efcf428507b064421ae
richard@iopen.net <richard@iopen.net>
2010-07-08 17:31:03 +1200

    In a weird move, go back from self.request to self.REQUEST because something seems to have changed

--- Products/CustomUserFolder/CustomUser.py
+++ Products/CustomUserFolder/CustomUser.py
@@ -387,7 +387,6 @@ class CustomUser(User, Folder):
         """
         self._properties = self._properties_def
     
-
     security.declareProtected(Perms.view, 'photo')
     def photo(self):
         """ Purely a helper method to get the image of a user.
@@ -396,7 +395,8 @@ class CustomUser(User, Folder):
         imageObject = self.get_image(url_only=False)
         if not imageObject:
             return None
-        self.request.response.setHeader('Cache-Control',
+        
+        self.REQUEST.response.setHeader('Cache-Control',
                                         'private; max-age=1200')
         
         if self.get_xsendfile_header():
@@ -420,12 +420,12 @@ class CustomUser(User, Folder):
     def get_xsendfile_header(self):
         sendfile_header = None
         # check that we're actually being called from a browser first
-        if not(hasattr(self, 'request')):
+        if not(hasattr(self, 'REQUEST')):
             sendfile_header = None
-        elif self.request.has_key('X-Sendfile-Type'):
-            sendfile_header = self.request.get('X-Sendfile-Type')
-        elif self.request.has_key('HTTP_X_SENDFILE_TYPE'):
-            sendfile_header = self.request.get('HTTP_X_SENDFILE_TYPE')
+        elif self.REQUEST.has_key('X-Sendfile-Type'):
+            sendfile_header = self.REQUEST.get('X-Sendfile-Type')
+        elif self.REQUEST.has_key('HTTP_X_SENDFILE_TYPE'):
+            sendfile_header = self.REQUEST.get('HTTP_X_SENDFILE_TYPE')
         
         return sendfile_header
 
@@ -448,13 +448,13 @@ class CustomUser(User, Folder):
                     gsimage = GSImage(f)
                     cache_path = gsimage.get_resized(81, 108, True,
                                                      return_cache_path=True)
-                    self.request.response.setHeader('Content-Type',
+                    self.REQUEST.response.setHeader('Content-Type',
                                                     gsimage.contentType)
                     if cache_path:
-                        self.request.response.setHeader(sendfile_header,
+                        self.REQUEST.response.setHeader(sendfile_header,
                                                 cache_path)
                     else:
-                        self.request.response.setHeader(sendfile_header,
+                        self.REQUEST.response.setHeader(sendfile_header,
                                                 imagePath)
                     retval = 'image'
                 else:

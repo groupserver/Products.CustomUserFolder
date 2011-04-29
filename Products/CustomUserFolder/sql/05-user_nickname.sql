fae3ccb5b03db85d0042dabfc16ff53047c460f3
Michael JasonSmith <mpj17@onlinegroups.net>
2011-04-29 14:53:51 +1000

    Shutting the SQL up a bit

--- Products/CustomUserFolder/sql/05-user_nickname.sql
+++ Products/CustomUserFolder/sql/05-user_nickname.sql
@@ -1,3 +1,7 @@
+SET CLIENT_ENCODING = 'UTF8';
+SET CHECK_FUNCTION_BODIES = FALSE;
+SET CLIENT_MIN_MESSAGES = WARNING;
+
 CREATE TABLE user_nickname (
     USER_ID           TEXT                        NOT NULL,
     NICKNAME          TEXT                        PRIMARY KEY,

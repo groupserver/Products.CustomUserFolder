0d8cecf70ae3fd0f56b6d62ea84fa52a6bc24b22
Michael JasonSmith <mpj17@onlinegroups.net>
2013-10-09 10:51:05 -0700

    In theory a PEP-8 cleanup, but this looks odd.

--- Products/CustomUserFolder/audituser.py
+++ Products/CustomUserFolder/audituser.py
@@ -1,53 +1,68 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2013 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from __future__ import absolute_import
 from datetime import date
 from zope.component import createObject
 from zope.component.interfaces import IFactory
 from zope.interface import implements, implementedBy
 from Products.GSAuditTrail import IAuditEvent, BasicAuditEvent, \
   AuditQuery, event_id_from_data
-from interfaces import IGSUserInfo
+from .interfaces import IGSUserInfo
+from .userinfo import userInfo_to_anchor
 
 SUBSYSTEM = 'groupserver.CustomUser'
 import logging
-log = logging.getLogger(SUBSYSTEM) #@UndefinedVariable
+log = logging.getLogger(SUBSYSTEM)
 
-UNKNOWN        = 0
+UNKNOWN = 0
 CHANGE_PROFILE = 1
+
 
 class CustomUserAuditEventFactory(object):
     implements(IFactory)
 
-    title=u'Custom User Audit-Event Factory'
-    description=u'Creates a GroupServer audit event for custom user'
+    title = u'Custom User Audit-Event Factory'
+    description = u'Creates a GroupServer audit event for custom user'
 
-    def __call__(self, context, eventId,  code, d,
-        userInfo, instanceUserInfo,  siteInfo,  
-        instanceDatum, supplementaryDatum):
+    def __call__(self, context, eventId, code, d, userInfo, instanceUserInfo,
+                siteInfo, instanceDatum, supplementaryDatum):
 
         if code == CHANGE_PROFILE:
-            event = SetPasswordEvent(context, eventId, d, 
-              userInfo, instanceUserInfo, siteInfo,
-              instanceDatum, supplementaryDatum)
+            event = ChangeProfileEvent(context, eventId, d, userInfo,
+                        instanceUserInfo, siteInfo, instanceDatum,
+                        supplementaryDatum)
         else:
-            event = BasicAuditEvent(context, eventId, UNKNOWN, d, 
-              userInfo, instanceUserInfo, siteInfo, None, 
-              instanceDatum, supplementaryDatum, SUBSYSTEM)
+            event = BasicAuditEvent(context, eventId, UNKNOWN, d, userInfo,
+                        instanceUserInfo, siteInfo, None, instanceDatum,
+                        supplementaryDatum, SUBSYSTEM)
         return event
-    
+
     def getInterfaces(self):
         return implementedBy(BasicAuditEvent)
+
 
 class ChangeProfileEvent(BasicAuditEvent):
     implements(IAuditEvent)
 
-    def __init__(self, context, id, d, userInfo, instanceUserInfo, 
-        siteInfo, instanceDatum,  supplementaryDatum):
-        
-        BasicAuditEvent.__init__(self, context, id, 
-          CHANGE_PROFILE, d, userInfo, instanceUserInfo, 
-          siteInfo, None,  instanceDatum, supplementaryDatum, 
-          SUBSYSTEM)
-    
+    def __init__(self, context, id, d, userInfo, instanceUserInfo,
+                siteInfo, instanceDatum, supplementaryDatum):
+
+        BasicAuditEvent.__init__(self, context, id, CHANGE_PROFILE, d,
+                                userInfo, instanceUserInfo, siteInfo, None,
+                                instanceDatum, supplementaryDatum, SUBSYSTEM)
+
     def __str__(self):
         retval = u'%s (%s) set password on %s (%s)' %\
           (self.instanceUserInfo.name, self.instanceUserInfo.id,
@@ -58,32 +73,35 @@ class ChangeProfileEvent(BasicAuditEvent):
     def xhtml(self):
         cssClass = u'audit-event profile-event-%s' % self.eventCode
         retval = u'<span class="%s">%s set password</span>' % \
-          (cssClass, 
+          (cssClass,
            userInfo_to_anchor(self.instanceUserInfo),
            self.instanceDatum)
         return retval
 
+
+# --=mpj17=-- WTF?
+
 def userInstanceModified(instance, event):
     log.info('Stuff')
+
 
 class foo(object):
     def __init__(self, user):
         self.user = user
-        self.userInfo = createObject('groupserver.LoggedInUser',user)
+        self.userInfo = createObject('groupserver.LoggedInUser', user)
         self.instanceUserInfo = IGSUserInfo(user)
         self.siteInfo = createObject('groupserver.SiteInfo', user)
-        
+
         self.queries = AuditQuery()
-        self.factory = ProfileAuditEventFactory()
-        
-    def info(self, code, instanceDatum = '', supplementaryDatum = ''):
+        self.factory = CustomUserAuditEventFactory()
+
+    def info(self, code, instanceDatum='', supplementaryDatum=''):
         d = date.today()
-        eventId = event_id_from_data(self.userInfo, 
+        eventId = event_id_from_data(self.userInfo,
           self.instanceUserInfo, self.siteInfo, code, instanceDatum,
           supplementaryDatum)
-        e = self.factory(self.user, eventId,  code, d,
+        e = self.factory(self.user, eventId, code, d,
           self.userInfo, self.instanceUserInfo, self.siteInfo,
           instanceDatum, supplementaryDatum)
         self.queries.store(e)
         log.info(e)
-

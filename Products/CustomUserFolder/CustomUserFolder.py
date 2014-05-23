e7cbdbf8bc9d97cc683b9aa1ae4c15ef2ed5dfa9
Michael JasonSmith <mpj17@onlinegroups.net>
2014-05-23 13:25:16 -0700

    Whitespace fix

--- Products/CustomUserFolder/CustomUserFolder.py
+++ Products/CustomUserFolder/CustomUserFolder.py
@@ -18,7 +18,8 @@
 # You MUST follow the rules in http://iopen.net/STYLE before checking in code
 # to the trunk. Code which does not follow the rules will be rejected.
 #
-import os, DateTime, re
+import os
+import DateTime
 import sqlalchemy as sa
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
 from AccessControl import ClassSecurityInfo
@@ -37,68 +38,62 @@ from gs.database import getTable, getSession
 import logging
 log = logging.getLogger('CustomUserFolder')
 
-try:
-    # richard: mailboxer was merged into XWFMailingListManager
-    from Products.XWFMailingListManager import MailBoxerTools
-    HaveMailBoxer = True
-except:
-    HaveMailBoxer = False
 
 class CustomUserFolder(UserFolderWithGroups):
     """ A user folder for CampusUser users, based on the NuxUserGroup UserFolder
     interface.
-    
+
     """
     implements(ICustomUserFolder)
 
     security = ClassSecurityInfo()
-    
+
     meta_type = "Custom User Folder"
-    id = 'acl_users'
+    id = 'acl_users'  # lint:ok
     title = 'Custom User Folder'
     icon = 'p_/UserFolder'
-    
+
     security.declarePrivate('_mainUser')
     _mainUser = PageTemplateFile('zpt/mainUser.zpt', globals(),
                                  __name__='manage_main')
     manage = manage_main = _mainUser
-    
+
     def __init__(self, user_folder_id):
         """ Initialize the data storage.
-           
+
         """
         self.user_folder_id = user_folder_id
         UserFolderWithGroups.__init__(self)
-        
+
     def _encryptPassword(self, pw):
         # we need to override the default, because if we encrypt with SSHA
         # we have trouble when we do the wire protocol
         return AuthEncoding.pw_encrypt(pw, 'SHA')
-        
+
     security.declarePrivate('_getUserFolder')
     def _getUserFolder(self):
         """ Return the folder object that contains the user objects,  or None.
-        
+
         """
         return getattr(self, self.user_folder_id, None)
 
     security.declareProtected(Perms.manage_users, 'get_userIdByEmail')
     def get_userIdByEmail(self, email):
         """ Get a user ID by email address.
-        
+
         """
         email = email.lower()
         uet = getTable('user_email')
         s = sa.select([uet.c.user_id], limit=1)
         s.append_whereclause(email == sa.func.lower(uet.c.email))
-        
+
         session = getSession()
         r = session.execute(s).fetchone()
         if r:
             return r['user_id']
 
         return None
-        
+
     security.declareProtected(Perms.manage_users, 'get_userByEmail')
     def get_userByEmail(self, email):
         """ Get the user by email address.
@@ -113,7 +108,7 @@ class CustomUserFolder(UserFolderWithGroups):
     def get_userIdByEmailVerificationId(self, verificationId):
         m = 'Use gs.profile.email.verify'
         assert False, m
-        
+
     security.declareProtected(Perms.manage_users, 'get_userByEmailVerificationId')
     def get_userByEmailVerificationId(self, verificationId):
         """ Get the user by verification ID"""
@@ -139,22 +134,22 @@ class CustomUserFolder(UserFolderWithGroups):
     def get_userByPasswordVerificationId(self, verificationId):
         m = 'Use gs.profile.password'
         assert False, m
-        
+
     security.declareProtected(Perms.manage_users, 'get_userByEmail')
     def getUserNames(self):
         """ Return a list of usernames.
-        
+
         """
         user_folder = self._getUserFolder()
         names = list(user_folder.objectIds('Custom User'))
         names.sort()
-        
+
         return names
-    
+
     security.declareProtected(Perms.manage_users, 'getUsers')
     def getUsers(self):
         """ Return a list of user objects.
-        
+
         """
         user_folder = self._getUserFolder()
         users = list(user_folder.objectValues('Custom User'))
@@ -162,16 +157,16 @@ class CustomUserFolder(UserFolderWithGroups):
         for user in users:
             user_list.append(user.__of__(self))
         return user_list
-    
+
     security.declareProtected(Perms.manage_users, 'getUser')
     def getUser(self, name):
         """ Return the named user object or None.
-        
+
         """
         if not name:
             log.error("User ID was not set in getUser")
             return None
-        
+
         assert type(name) in (str, unicode), \
           'User ID is a %s not a string (%s)' % (type(name), name)
         user_folder = self._getUserFolder()
@@ -179,27 +174,27 @@ class CustomUserFolder(UserFolderWithGroups):
         if user:
             return user.__of__(self)
         return None
-    
+
     security.declarePrivate('_doAddUser')
     def _doAddUser(self, name, password, roles, domains, groups=(), **kw):
         """ Create a new user.
-        
+
         """
         import CustomUser
-        
+
         user_folder = self._getUserFolder()
         if password is not None and self.encrypt_passwords:
             password = self._encryptPassword(password)
-                    
+
         CustomUser.addCustomUser(user_folder, name, password, roles, domains)
         self.setGroupsOfUser(groups, name)
-        
+
         user = self.getUser(name)
-        
+
         user.init_properties()
         user.title = name
         user.manage_setLocalRoles(name, ['Owner'])
-        
+
         return 1
 
     security.declarePrivate('_doChangeUser')
@@ -211,7 +206,7 @@ class CustomUserFolder(UserFolderWithGroups):
             user.__ = password
         user.roles = roles
         user.domains = domains
-        
+
         if groups is not None:
             self.setGroupsOfUser(groups, name)
 
@@ -219,7 +214,7 @@ class CustomUserFolder(UserFolderWithGroups):
     def _doDelUsers(self, names):
         user_folder = self._getUserFolder()
         user_folder.manage_delObjects(names)
-        
+
         for username in names:
             user = self.getUser(username)
             if user is None:
@@ -243,33 +238,33 @@ class CustomUserFolder(UserFolderWithGroups):
 
         user.manage_changeProperties(fn=displayName)
         # For now
-        user.manage_addProperty('creation_date', DateTime.DateTime(), 
+        user.manage_addProperty('creation_date', DateTime.DateTime(),
                                 'date')
-  
+
         assert user
         assert userId == user.getId()
         assert displayName == user.getProperty('fn')
         return user
-        
+
     security.declareProtected(Perms.manage_users, 'wf_manage_users')
     def wf_manage_users(self, submit=None, REQUEST=None, RESPONSE=None):
-        """ A helper submission method for the modified ZMI interface, 
+        """ A helper submission method for the modified ZMI interface,
         to handle the multiple selection box.
-        
+
         """
         if submit == 'Edit':
             try:
                 REQUEST['name'] = REQUEST['names'][0]
             except:
                 pass
-                
+
         return self.manage_users(submit, REQUEST, RESPONSE)
 
     security.declarePublic('verify_address')
     def verify_address(self, Mail):
         m = 'Use gs.profile.email.verify'
         assert False, m
-        
+
     security.declarePublic('get_userIdByNickname')
     def get_userIdByNickname(self, nickname):
         assert nickname
@@ -277,7 +272,7 @@ class CustomUserFolder(UserFolderWithGroups):
         unt = getTable('user_nickname')
         s = sa.select([unt.c.user_id], limit = 1)
         s.append_whereclause(unt.c.nickname == nickname)
-        
+
         session = getSession()
         r = session.execute(s)
         retval = ''
@@ -309,7 +304,7 @@ class CustomUserFolder(UserFolderWithGroups):
         if len(self.data) <= 1:
             info = readUserAccessFile('inituser')
             if info:
-                name, password, domains, remote_user_mode = info #@UnusedVariable
+                name, password, domains, remote_user_mode = info
                 self._doDelUsers(self.getUserNames())
                 self._doAddUser(name, password, ('Manager',), domains)
                 try:
@@ -320,7 +315,7 @@ class CustomUserFolder(UserFolderWithGroups):
     security.declareProtected(Perms.view, 'migrate_images')
     def migrate_images(self):
         """ Migrate the images to disk
-        
+
         """
         siteId = self.site_root().getId()
         contactImageDir = locateDataDirectory("groupserver.user.image",
@@ -328,7 +323,7 @@ class CustomUserFolder(UserFolderWithGroups):
         for image in self.contactsimages.objectValues():
             if image.meta_type == 'Image':
                 filePath = os.path.join(contactImageDir, image.getId())
-                f = file(filePath, 'a+') 
+                f = file(filePath, 'a+')
                 f.write(str(image.data))
                 f.close()
 
@@ -337,6 +332,7 @@ manage_addCustomUserFolderForm = PageTemplateFile(
     globals(),
     __name__='manage_addCustomUserFolderForm')
 
+
 def manage_addCustomUserFolder(self, id='users', REQUEST=None):
     """ Add a CustomUserFolder to a container as acl_users.
 
@@ -344,9 +340,10 @@ def manage_addCustomUserFolder(self, id='users', REQUEST=None):
     ob = CustomUserFolder(id)
     self._setObject('acl_users', ob)
     if REQUEST is not None:
-        return self.manage_main(self,REQUEST)
-    
+        return self.manage_main(self, REQUEST)
+
 InitializeClass(CustomUserFolder)
+
 
 def initialize(context):
     context.registerClass(
@@ -356,4 +353,3 @@ def initialize(context):
                       manage_addCustomUserFolder,),
         icon='icons/customuserfolder.gif'
         )
-

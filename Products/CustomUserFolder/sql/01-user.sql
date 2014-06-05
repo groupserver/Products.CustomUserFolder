dc489fffa65198da48c3f114922ecfa416a0935a
Michael JasonSmith <mpj17@onlinegroups.net>
2014-06-04 22:45:41 -0700

    Moving the CREATE TABLE SQL for the email-settings to gs.group.member.email.base

--- Products/CustomUserFolder/sql/01-user.sql
+++ Products/CustomUserFolder/sql/01-user.sql
@@ -1,31 +1,12 @@
 SET CLIENT_ENCODING = 'UTF8';
-SET CHECK_FUNCTION_BODIES = FALSE;
 SET CLIENT_MIN_MESSAGES = WARNING;
 
--- TODO: Move to gs.group.member.base
-
-CREATE TABLE GROUP_USER_EMAIL (
-    USER_ID           TEXT                      NOT NULL,
-    SITE_ID	      TEXT			NOT NULL DEFAULT ''::TEXT,
-    GROUP_ID          TEXT			NOT NULL,
-    EMAIL             TEXT                      NOT NULL REFERENCES USER_EMAIL (EMAIL) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-CREATE UNIQUE INDEX user_site_group_email_pkey ON group_user_email USING BTREE
-       (user_id, site_id, group_id, lower(email));
-
-CREATE TABLE EMAIL_SETTING (
-    USER_ID           TEXT                      NOT NULL,
-    SITE_ID	      TEXT			NOT NULL DEFAULT ''::TEXT,
-    GROUP_ID          TEXT			NOT NULL DEFAULT ''::TEXT,
-    SETTING	      TEXT			NOT NULL
-);
-
-CREATE UNIQUE INDEX email_setting_pkey ON email_setting USING BTREE
-       (user_id, site_id, group_id);
-
--- These are email addresses that are *never* allowed to participate in the system
-CREATE TABLE EMAIL_BLACKLIST (
-	EMAIL        TEXT        NOT NULL
+-- These are email addresses that are *never* allowed to
+-- participate in the system. Oddly enough, while it is declared
+-- here (for sentemental reasons) it is only ever used by
+-- Products.XWFMailingListManager. It probabily should be
+-- refactored, along with the mailing-list manager.
+CREATE TABLE email_blacklist (
+    email  TEXT  NOT NULL
 );
 
